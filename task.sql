c55f9193b5b168d18e2b590cc7b18e3f53b72c7d
Volkov <volkovyegorandriiovich@gmail.com>
2024-05-23 15:09:32 +0300

    Stored procedure script ready

--- task.sql
+++ task.sql
@@ -1,3 +1,16 @@
 USE ShopDB; 
 
 -- Create your stored procedure here
+
+DELIMITER //
+CREATE PROCEDURE get_warehouse_product_inventory( 
+	IN WarehouseIDFilter varchar(10)
+)
+BEGIN
+    SELECT Products.Name AS 'Product Name', ProductInventory.WarehouseAmount AS 'Amount'
+	FROM Products
+	INNER JOIN ProductInventory
+	ON Products.ID = ProductInventory.ProductID
+	WHERE ProductInventory.WarehouseID=WarehouseIDFilter;
+END //
+DELIMITER ;
