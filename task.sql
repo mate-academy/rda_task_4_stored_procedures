d0de23e062cb196eecb0dc1f2b72bc3ee2cebb08
Darya-Savchenko <savchenkodarya277@gmail.com>
2024-07-17 18:34:28 +0300

    solution

--- task.sql
+++ task.sql
@@ -1,3 +1,14 @@
 USE ShopDB; 
 
--- Create your stored procedure here
+DELIMITER //
+CREATE PROCEDURE get_warehouse_product_inventory(
+	IN WarehouseIDFilter INT
+)
+BEGIN
+    SELECT SUM(WarehouseAmount) AS TotalAmount, Products.Name AS ProductName
+    FROM ProductInventory
+    JOIN Products ON ProductInventory.ProductID = Products.ID
+    WHERE WarehouseID = WarehouseIDFilter
+    GROUP BY Products.Name;
+END //
+DELIMITER ;
