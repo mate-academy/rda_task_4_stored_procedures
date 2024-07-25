c35bb1e23c84d389728bdb0110d1248c2513663c
elfredburger <elfredfredburger@gmail.com>
2024-07-25 17:33:54 +0200

    Solution

--- task.sql
+++ task.sql
@@ -1,8 +1,12 @@
 USE ShopDB;
 
+DELIMITER //
+
 CREATE PROCEDURE get_warehouse_product_inventory( 
 	IN IDFilter INT
 )
 BEGIN
     SELECT WarehouseAmount FROM ProductInventory WHERE WarehouseID=IDFilter;
-END;
+END //
+
+DELIMITER;
