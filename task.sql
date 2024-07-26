04857a3723579a10af699d7dbf47bf70084a996a
silchenko-arsen <silchenkoarsen@gmail.com>
2024-07-26 14:45:07 +0300

    fix code

--- task.sql
+++ task.sql
@@ -5,7 +5,7 @@ CREATE PROCEDURE get_warehouse_product_inventory(
     IN WarehouseIDFilter INT
 )
 BEGIN
-SELECT ProductInventory.WarehouseAmount AS TotalAmount, Products.Name AS ProductName
+SELECT SUM(WarehouseAmount) AS TotalAmount, Products.Name AS ProductName
 FROM ProductInventory
          JOIN Products ON ProductInventory.ProductID = Products.ID
 WHERE WarehouseID = WarehouseIDFilter
