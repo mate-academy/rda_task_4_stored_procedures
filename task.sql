836abe5e06db3f0f3b2f5be5c33e94c368ecfc51
Ann <grotesski@gmail.com>
2024-09-03 14:56:29 +0300

    Solution

--- task.sql
+++ task.sql
@@ -2,6 +2,7 @@ USE ShopDB;
 
 -- Create your stored procedure here
 DELIMITER //
+
 CREATE PROCEDURE get_warehouse_product_inventory(IN ID INT)
 BEGIN
     SELECT Products.Name AS ProductName, ProductInventory.WarehouseAmount AS Amount
@@ -10,4 +11,5 @@ BEGIN
     ON ProductInventory.ProductID = Products.ID
     WHERE ProductInventory.WarehouseID = ID;
 END //
-DELIMITER;
+
+DELIMITER ;
