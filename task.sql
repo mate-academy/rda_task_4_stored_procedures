d2c4069bc5131992699d5d50a1ac56e05e231476
Vicheslav Der <efirshey@gmail.com>
2024-09-16 21:47:39 +0300

    solution

--- task.sql
+++ task.sql
@@ -1,3 +1,17 @@
 USE ShopDB; 
 
 -- Create your stored procedure here
+
+DELIMITER //
+CREATE PROCEDURE get_warehouse_product_inventory(
+    IN WarehouseID INT
+)
+
+BEGIN
+    SELECT Products.name, ProductInventory.WarehouseAmount;
+    FROM Products
+    INNER JOIN ProductInventory
+    ON ProductID = ProductInventory.ProductID
+    WHERE ProductInventory.WarehouseID = WarehouseID;
+END //
+DELIMITER ;
