5ea889328687b3ec017ad6b2c52b94820df6adc9
88victory88 <victory.otrok@gmail.com>
2025-01-08 20:33:57 +0800

    Solution2

--- task.sql
+++ task.sql
@@ -4,10 +4,10 @@ DELIMITER $$
 CREATE PROCEDURE get_warehouse_product_inventory(IN WarehouseID INT)
 BEGIN
     SELECT
-        Name AS ProductName,
+        ProductID AS ProductName,
         WarehouseAmount AS Amount
     FROM
-        Products;
+        ProductInventory;
 END$$
 
 DELIMITER ;
