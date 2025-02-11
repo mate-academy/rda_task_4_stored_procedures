3144f2b0116391a86bc9809d7200fe99804c5506
Taras Kudinov <38283923+tkudinov@users.noreply.github.com>
2025-02-11 14:47:07 +0200

    Added changes to DB execution script.

--- task.sql
+++ task.sql
@@ -7,6 +7,7 @@ CREATE PROCEDURE get_warehouse_product_inventory(
 )
 BEGIN
     SELECT Products.Name, ProductInventory.WarehouseAmount FROM Products INNER JOIN ProductInventory
+    ON Products.ID = ProductInventory.ProductID
     WHERE ProductInventory.WarehouseID=WarehouseID;
 END //
 DELIMITER ;
